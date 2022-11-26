836cc6a032e358da0570ac80dc9c437599d5a7cb
sugiartofahmi <sugiartofahmi@gmail.com>
2022-11-27 00:59:15 +0700

    Fahmi Sugiarto | Fix Select Option

--- src/components/Select/index.jsx
+++ src/components/Select/index.jsx
@@ -2,7 +2,6 @@ const Select = ({ data, onChange, value }) => {
   return (
     <>
       <select
-        value={value}
         onChange={onChange}
         className="text-gray-700 text-sm  border  rounded-lg  w-full p-2.5"
       >
